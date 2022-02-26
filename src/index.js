a672fd81316de07daa7a8b49bb4b24a2b5073184
kmadhav907 <madhavbhat@Madhavs-MacBook-Air.local>
2022-02-26 16:04:36 +0530

    some commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import reportWebVitals from './reportWebVitals';
+const App   = React.createElement("h1", {style: {fontSize: 45, color: 'red'}}, "Hello world")
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {App}
   </React.StrictMode>,
   document.getElementById('root')
 );
